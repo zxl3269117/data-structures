00c9421552577b8f65f6e8727e9151e69cc9f21c
Ziqian Li <liziqian1992@gmail.com>
2022-02-27 22:53:18 -0800

    Complete functional stack and queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,28 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    // adding the inputting value to the storage with numeric keys
+    storage[someInstance.size(storage)] = value;
   };
 
   someInstance.dequeue = function() {
+    var k = 0;
+    var firstItem = storage[0];
+    var temp = {};
+    while (storage[k + 1]) {
+      temp[0] = storage[k + 1];
+      k ++;
+    }
+    storage = temp;
+    return firstItem;
   };
 
   someInstance.size = function() {
+    var count = 0;
+    while (storage[count]) {
+      count ++;
+    }
+    return count;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,27 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[someInstance.size(storage)] = value;
   };
 
   someInstance.pop = function() {
+    var k = 0;
+    var temp = {};
+    var lastItem = storage[someInstance.size(storage) - 1];
+    while (k < someInstance.size(storage) - 1) {
+      temp[k] = storage[k];
+      k ++;
+    }
+    storage = temp;
+    return lastItem;
   };
 
   someInstance.size = function() {
+    var count = 0;
+    while (storage[count]) {
+      count ++;
+    }
+    return count;
   };
 
   return someInstance;
