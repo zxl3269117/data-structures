d77b7e08cefefd7e5bd27ff011f1b9d1102559dc
Ziqian Li <liziqian1992@gmail.com>
2022-03-01 20:03:29 -0800

    Complete sprint one

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,27 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
 
+  enqueue(value) {
+    this.storage[this.size()] = value;
+  }
+  dequeue() {
+    var firstItem = this.storage[0];
+    var index = 1;
+    while (this.storage[index]) {
+      this.storage[index - 1] = this.storage[index];
+      index ++;
+    }
+    delete this.storage[index - 1];
+    return firstItem;
+  }
+  size() {
+    var count = 0;
+    while (this.storage[count]) {
+      count ++;
+    }
+    return count;
+  }
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,23 @@
 class Stack {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
 
+  push(value) {
+    this.storage[this.size()] = value;
+  }
+
+  pop() {
+    var poppedItem = this.storage[this.size() - 1];
+    delete this.storage[this.size() - 1];
+    return poppedItem;
+  }
+
+  size() {
+    var count = 0;
+    while (this.storage[count]) {
+      count ++;
+    }
+    return count;
+  }
 }
